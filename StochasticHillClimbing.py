f53045e23061e0b7ed84caa1ec2346723b92217a
Fadhil <Undead.gamer313@gmail.com>
2024-11-05 09:29:26 +0700

    benerin stochastic

--- StochasticHillClimbing.py
+++ StochasticHillClimbing.py
@@ -1,7 +1,8 @@
 from Creating import *
 import random
 import datetime
-
+import cProfile
+import re
 max_iteration = int(input("banyak iterasi = "))
 max_duration = int(input("duration = "))
 
@@ -22,11 +23,12 @@ def stochastic_hill_climbing(list):
             end = (datetime.datetime.now() - start).seconds
             if end >= max_duration:
                 break
-
-        current_list = neighbor_list
-        neighbor_list = neighbor(current_list)
-        current_value = objectivefunction(current_list)
-        neighbor_value = objectivefunction(neighbor_list)
+        
+        if neighbor_value>current_value :
+            current_list = neighbor_list
+            neighbor_list = neighbor(current_list)
+            current_value = objectivefunction(current_list)
+            neighbor_value = objectivefunction(neighbor_list)
         
         if end >= max_duration:
             break
@@ -34,14 +36,17 @@ def stochastic_hill_climbing(list):
     return current_list
 
 
-start = datetime.datetime.now()
-initialList = initial()
-a = objectivefunction(initialList)
-print("CURRENT SCORE " + str(a))
-print(initialList)
-finalList = stochastic_hill_climbing(initialList)
-b = objectivefunction(finalList)
-print("FINAL SCORE " + str(b))
-print(finalList)
-end = (datetime.datetime.now() - start)
-print("Total waktu : ", end)
+def main():
+    start = datetime.datetime.now()
+    initialList = initial()
+    a = objectivefunction(initialList)
+    print("CURRENT SCORE " + str(a))
+    print(initialList)
+    finalList = stochastic_hill_climbing(initialList)
+    b = objectivefunction(finalList)
+    print("FINAL SCORE " + str(b))
+    print(finalList)
+    end = (datetime.datetime.now() - start)
+    print("Total waktu : ", end)
+
+cProfile.run("main()")
